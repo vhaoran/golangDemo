0ac8b41f74b7dc01a11968d02abb2ddbcb2fdf45
weihaoranW <weihot2005@163.com>
2018-09-02 16:06:43 +0800

    ---

---
+++ base/02.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	//	"github.com/gin-gonic/gin"
+	//"myblog/g/xbind"
+	//	"myblog/db"
+	//"log"
+	"fmt"
+	//"strconv"
+	//"strings"
+	//"reflect"
+	//"time"
+	//	"net/http"
+	//	"bytes"
+	//	"encoding/binary"
+	//	"github.com/widuu/goini"
+	//      _ "github.com/go-sql-driver/mysql"
+	//      "database/sql/driver"
+)
+
+func main() {
+	fmt.Println("--------------")
+
+}
