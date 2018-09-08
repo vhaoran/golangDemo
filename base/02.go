376b61752b266af968b4716a96d1bafcf6d80a6b
weihaoranW <weihot2005@163.com>
2018-09-08 18:37:39 +0800

    _

--- base/02.go
+++ base/02.go
@@ -1,24 +1,34 @@
 package main
 
+// deom for
+
 import (
-	//	"github.com/gin-gonic/gin"
-	//"myblog/g/xbind"
-	//	"myblog/db"
-	//"log"
 	"fmt"
-	//"strconv"
-	//"strings"
-	//"reflect"
-	//"time"
-	//	"net/http"
-	//	"bytes"
-	//	"encoding/binary"
-	//	"github.com/widuu/goini"
-	//      _ "github.com/go-sql-driver/mysql"
-	//      "database/sql/driver"
 )
 
 func main() {
 	fmt.Println("--------------")
+	for i := 0; i < 8; i++ {
+		fmt.Println(i)
+	}
+	//
+	fmt.Println("----------------")
+	lst := [5]int{3, 4, 5, 6, 7}
+	for i, each := range lst {
+		fmt.Println(i, each)
+	}
+	//
+	fmt.Println("----")
+	for _, each := range lst {
+		fmt.Println(each)
+	}
 
+	//
+	i := 0
+	for {
+		fmt.Println("##")
+		if i = i + 1; i > 5 {
+			break
+		}
+	}
 }
